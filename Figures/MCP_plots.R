91f833ea373e6a91431bd5f3f17b2d9ab86959ac
jsekamane <jonassekamane@Jonass-MacBook-Pro.local>
2016-01-11 12:10:01 +0100

    Figures: restructured in to ENP and Mean eccentricity section

--- Figures/MCP_plots.R
+++ Figures/MCP_plots.R
@@ -1,34 +1,43 @@
 library(ggplot2)
 
 setwd("~/Dropbox/Economics/Courses/Thesis/Positioning/Figures")
-#data = read.csv("data/MCP_hunter_mean_eccentricity_20160110_184957_i250_b150.csv")
-data = read.csv("data/MCP_hunter_ENP_20160110_184957_i250_b150.csv")
 
-# Groups the polarization variable mu into three groups.
+
+
+### Mean eccentricity ###
+data = read.csv("data/MCP_hunter_mean_eccentricity_20160110_210442_i250_b150.csv")
+
 data[, "polarization"] = NA
 data[data$mu <= 0.5, ][, "polarization"] = 1
 data[0.5 < data$mu & data$mu < 1, ][, "polarization"] = 2
 data[1 <= data$mu, ][, "polarization"] = 3
 
-
-# Mean eccentricity
-# Geoms and stats are automatically split by aesthetics that are factors
-c = ggplot(data, aes(y=MeanEst, x=N, colour=factor(polarization)))
-c + stat_smooth() + 
+c1 = ggplot(data, aes(y=MeanEst, x=N, colour=factor(polarization)))
+c1 + stat_smooth() + 
   geom_point() + 
   xlim(2,12) + 
   ylim(0, 1.65)
 
-# Geoms and stats are automatically split by aesthetics that are factors
-c2 = ggplot(data, aes(y=MeanEst, x=n_ratio, colour=factor(N)))
-c2 + stat_smooth() + geom_point()
+# Subset
+datasubset = subset(data, N %in% c(2,3,4,12))
+
+c2 = ggplot(datasubset, aes(y=MeanEst, x=n_ratio, colour=factor(N)))
+c2 + stat_smooth(aes(fill = factor(N)))# + 
+     #geom_point()
 
 
-# ENP
-# Geoms and stats are automatically split by aesthetics that are factors
-c = ggplot(data, aes(y=MeanEst, x=N, colour=factor(polarization)))
-c + stat_smooth() + 
-  geom_point() + 
+
+### ENP ###
+data = read.csv("data/MCP_hunter_ENP_20160110_210442_i250_b150.csv")
+
+data[, "polarization"] = NA
+data[data$mu <= 0.5, ][, "polarization"] = 1
+data[0.5 < data$mu & data$mu < 1, ][, "polarization"] = 2
+data[1 <= data$mu, ][, "polarization"] = 3
+
+c3 = ggplot(data, aes(y=MeanEst, x=N, colour=factor(polarization)))
+c3 + stat_smooth() + 
   geom_abline(intercept = 0, slope = 1) +
   xlim(2,12) + 
-  ylim(1, 12) 
+  ylim(1, 12)# +
+  #geom_point()
